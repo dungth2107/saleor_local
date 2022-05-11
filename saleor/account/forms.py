b3cfacd9f8ade92b5e4c28fcbdcd1066738567b6
dungth <dungth@smartosc.com>
2022-05-11 11:23:09 +0700

    test 2

--- saleor/account/forms.py
+++ saleor/account/forms.py
@@ -16,6 +16,7 @@ def get_address_form(data, country_code, initial=None, instance=None, **kwargs):
         initial["phone"] = "+{}".format(country_code_for_region(country_code))
         print(123213)
         print(44444)
+        print(5555)
 
     address_form_class = get_address_form_class(country_code)
 
